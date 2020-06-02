ace647955cd4f4179921bfb185adc5d488b7db7b
yure <yurematias@ufrn.edu.br>
2020-06-02 19:23:15 -0300

    traduçao do index finalizada

--- js/english.js
+++ js/english.js
@@ -11,7 +11,7 @@ export default {
   Agricultural School of Jundiaí in Federal 
   University of Rio Grande do Norte, Brazil.
   Consists in a hardware based application for aeration / drying of grains in storage silos in a IoT context.`,
-  justificativa: 'Justificativa',
+  justificativa: 'Justification',
   textoJustificativa: `With the increase in productivity presented by the grain sector in the country, the need for not only
   storage of products, but that they are optimized to provide greater durability of these products, it is
   of national and strategic interest for the primary economy chain. This project will allow the
@@ -19,14 +19,14 @@ export default {
   instrumentation in the context of Internet of Things (IoT), with the possibility not only of monitoring but of
   control variables remotely, to maintain humidity / temperature levels suitable for each type of
   grain and microclimate conditions inside and outside the silo installation site...`,
-  plano: 'Plano de trabalho',
+  plano: 'Work Plan',
   projetoDePesquisa: 'Research project:',
   orientador: 'Advisor: ',
   nomeOrientador: 'Josenalde Barbosa de Oliveira',
   centro: 'Center: ',
   nomeCentro: 'Agricultural School of Jundiaí',
   discente: 'Student: ',
-  areaDeConhecimento: 'Área de conhecimento',
+  areaDeConhecimento: 'Knowledge Area',
   grandeArea: 'Great area: ',
   engenharias: 'Engineering',
   area: 'Área: ',
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import instanciaVue from './instanciaVue.js';
 import ptbr from './ptbr.js';
 
-setTimeout(() => carregarConteudo(instanciaVue), 100);;
+setTimeout(() => carregarConteudo(instanciaVue), 1000);
 
 function carregarConteudo(vueApp) {
   Object.keys(ptbr).forEach(chave => {
